5c84bbcb4696157147bf4b0622783550dcebc843
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2023-06-30 19:03:46 +0800

    注解为“值”时值为-等同变量名。赋予时可通过“管道接”把名和值都回传利用

--- src/set_.rs
+++ src/set_.rs
@@ -1,5 +1,8 @@
 use super::{u_::*, as_ref__};
 
+pub const PRIV_:&str = "私";
+pub const VAL_:char = '值';
+
 pub struct Item_ {
 	pub super_:code_::Item1_,
 	pub names_:code_::OL_,
@@ -16,6 +19,7 @@ impl Item_ {
 	
 	pub fn hello2_1__(&self, names:T_<result_::List_>, vals:T_<result_::List_>, env:&code_::Env_) -> Result2_ {
 		t_::o__(&self.names_).hello__(&code_::Env_::new6(names, env))?;
+		#[cfg(debug_assertions)]
 		if as_ref__!(env.w).dbg_.lc_ {
 			use super::u_::code_::Item_;
 			as_ref__!(env.w).dbg_.lc_kw__(t_::or__(&self.super_.kw2__()));
@@ -28,6 +32,10 @@ impl Item_ {
 		self.hello2_1__(names.clone(), vals.clone(), env)?;
 		let names = as_ref__!(names);
 		let vals = as_ref__!(vals);
+		#[cfg(debug_assertions)]
+		if dbp2__!("-set-", env) {
+			lc3__!("\nnames {:?}\n vals {:?}\n",names.a_,vals.a_);
+		}
 		
 		let mut name = String::new();
 		let mut val = String::new();
@@ -38,7 +46,8 @@ impl Item_ {
 		let mut i_val = 0;
 		let mut cnt = 0;
 		let mut is_priv = false;
-		let mut douhao_in_end = false;
+		let mut gang_equ_name = false;
+		let mut dunhao_in_end = false;
 		loop {
 			name.clear();
 			let mut q2 = Some(env.q.clone());
@@ -49,23 +58,29 @@ impl Item_ {
 					break
 				}
 				i.s_inc_some_kw__(&mut name);
-				match self.super_.qv4rem__(&i.rem_, |rem| {
-					if rem == "私" {
-						is_priv = true;
-						return true
+				match code_::qv4rem__(&i.rem_, |rem| {
+					match rem {
+						PRIV_ => {
+							is_priv = true;
+							true
+						}
+						equ_name_::REM_ => {
+							gang_equ_name = true;
+							true
+						}
+						_ => false
 					}
-					false
 				}, q2.unwrap(), env.w.clone()) {
 					Ok(q3) => q2 = q3,
 					Err(e) => return e,
 				}
 			}
-			if i_val < vals.len() || douhao_in_end {
+			if i_val < vals.len() || dunhao_in_end {
 				val.clear();
 				val_rem.clear();
 				yes_val2 = false;
-				if douhao_in_end {
-					douhao_in_end = false
+				if dunhao_in_end {
+					dunhao_in_end = false
 				} else {
 					while i_val < vals.len() {
 						let i2 = &vals[i_val];
@@ -73,7 +88,7 @@ impl Item_ {
 						i_val += 1;
 						if i.dunhao__() {
 							if i_val == vals.len() {
-								douhao_in_end = true
+								dunhao_in_end = true
 							}
 							break
 						}
@@ -89,18 +104,31 @@ impl Item_ {
 					}
 				}
 			}
-			qv_::val2__(&name, if yes_val2 {val2.clone()} else {result_::sri__(&val, val_rem.clone())},
-				is_alias, is_priv, q2.unwrap().clone(), env.w.clone());
-			cnt += 1;
-			if env.gd.guandao_jie_ {
+			let val3 = if yes_val2 {val2.clone()} else {result_::sri__(&val, val_rem.clone())};
+			qv_::val2__(&name, val3.clone(), is_alias, is_priv, gang_equ_name, q2.unwrap().clone(), env.w.clone());
+			for c in env.gd.guandao_jie_.chars() {
+				cnt += 1;
 				if cnt > 1 {
 					as_ref__!(env.w).dunhao__(&mut as_mut_ref__!(env.ret));
 				}
-				as_mut_ref__!(env.ret).add__(&name);
+				match c {
+					'1' | '名' =>
+						as_mut_ref__!(env.ret).add__(&name),
+					VAL_ =>
+						as_mut_ref__!(env.ret).add4__(val3.clone()),
+					_ => return as_ref__!(env.w).no_guandaojie__(c)
+				}
 			}
 			if i_name >= names.len() {
 				break
 			}
+		}
+		//用后清除……如何个导致被存？
+		if let Some(i2) = names.end__() {
+			as_mut_ref__!(i2).rem_.clear();
+		}
+		if let Some(i2) = vals.end__() {
+			as_mut_ref__!(i2).rem_.clear();
 		}
 		ok__()
 	}
