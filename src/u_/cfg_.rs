e4cdb8a7775c7df9ad76090826dd1879131a2d65
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2023-06-30 19:04:59 +0800

    “视句号为顿号”

--- src/u_/cfg_.rs
+++ src/u_/cfg_.rs
@@ -2,10 +2,11 @@
 pub struct Cfg_ {
 	pub src_is_file_:bool,
 	pub shl_:String,
+	pub jvhao_dunhao_:bool,
 }
 
 impl Default for Cfg_ {
 	fn default() -> Self {
-		Self {src_is_file_:true, shl_:String::new()}
+		Self {src_is_file_:true, shl_:String::new(), jvhao_dunhao_:false}
 	}
 }
