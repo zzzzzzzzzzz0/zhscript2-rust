56f48b17dd1aea3b9b2c010f4c04adf9ade8f7cf
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2023-06-30 19:29:21 +0800

    更完善的文件路径搜索。解释结果的统一处理

--- src/u_/eval_.rs
+++ src/u_/eval_.rs
@@ -1,48 +1,50 @@
-use super::{*, super::{u2_::*, {lc3__, lc_kw__, p__, as_ref__, as_mut_ref__}}};
+use super::{*, super::{u2_::*, {as_ref__, as_mut_ref__}}};
+#[cfg(debug_assertions)]
+use super::super::{lc3__, lc_kw__, p__};
 use std::{fs::File, io::{Read, Error as ioError}, path::{PathBuf}};
 
 pub fn ok_src__(src:&str, q:qv_::T_, w:world_::T_) -> bool {
-	let shl = as_ref__!(w).cfg_.shl_.to_string();
-	let mut ok = false;
-	let op__ = |s:&str, ok:&mut bool| {
-		if as_ref__!(w).dbg_.path_ {
-			as_ref__!(w).dbg_.arg2__(s);
-		}
-		if File::open(s).is_ok() {
-			let mut q = as_mut_ref__!(q);
-			q.src_ = s.to_string();
-			q.src_is_file_ = true;
-			*ok = true;
-		}
-	};
-	op__(src, &mut ok);
-	if ok {
-		return ok
-	}
-	if !src.starts_with('/') {
-		let op2__ = |src3:&str, ok:&mut bool| {
-			let mut p = PathBuf::from(src3);
-			p.set_file_name(src);
-			if let Some(s) = p.to_str() {
-				op__(s, ok);
+	if src.is_empty() {
+		false
+	} else {
+		let op__ = |s:&str| {
+			#[cfg(debug_assertions)]
+			if as_ref__!(w).dbg_.path_ {
+				as_ref__!(w).dbg_.arg2__(s);
 			}
+			if File::open(s).is_ok() {
+				let mut q = as_mut_ref__!(q);
+				q.src_ = s.to_string();
+				q.src_is_file_ = true;
+				true
+			} else {false}
 		};
-		op2__(&shl, &mut ok);
-		if ok {
-			return ok
-		}
-		if qv_::for__(q.clone(), w.clone(), |q, _, _| {
-			let q2 = as_ref__!(q);
-			if q2.src_is_file_ {
-				op2__(&q2.src_, &mut ok);
-				if ok {
-					return Some(())
+		if src.starts_with('/') {
+		} else {
+			let op2__ = |src3:&str| {
+				let mut p = PathBuf::from(src3);
+				p.set_file_name(src);
+				if let Some(s) = p.to_str() {
+					op__(s)
+				} else {false}
+			};
+			for i in as_ref__!(w).path__() {
+				if op2__(i) {
+					return true
 				}
 			}
-			None
-		}).is_some() {return ok}
+			if qv_::for3__(q.clone(), w.clone(), |q, _, _| {
+				let q2 = as_ref__!(q);
+				if q2.src_is_file_ {
+					if op2__(&q2.src_) {
+						return Some(())
+					}
+				}
+				None
+			}, true, true, false).is_some() {return true}
+		}
+		op__(src)
 	}
-	ok
 }
 
 pub fn src__(src2:&mut String, q:qv_::T_, w:world_::T_) -> Result2_ {
@@ -57,8 +59,8 @@ pub fn src__(src2:&mut String, q:qv_::T_, w:world_::T_) -> Result2_ {
 					if src2.starts_with("#!") {
 						if let Some(i) = src2.find('\n') {
 							let mut q = as_mut_ref__!(q);
-							world_::clpars__(&mut as_mut_ref__!(w), &mut str_::split__(&src2[2..i]).into_iter(),
-								true, false, false, &mut q)?;
+							world_::clpars__(&mut str_::split__(&src2[2..i]).into_iter(),
+								true, false, false, true, &mut q, w)?;
 							*src2 = src2[i+1..].to_string();
 						}
 					}
@@ -69,6 +71,19 @@ pub fn src__(src2:&mut String, q:qv_::T_, w:world_::T_) -> Result2_ {
 		}
 		Err(e) => err__(e)
 	}
+}
+
+pub fn return__(ret:Result2_, env:&code_::Env_) -> Result2_ {
+	if let Err((jump_::RETURN_, _, s, _)) = &ret {
+		#[cfg(debug_assertions)]
+		if as_ref__!(env.w).dbg_.lc_ {
+			lc3__!("({}={:?})", s, as_ref__!(env.q).name_);
+		}
+		if s.is_empty() || as_ref__!(env.q).name_.contains(s) {
+			return ok__()
+		}
+	}
+	ret
 }
 
 pub fn hello__(src:&str, env:&code_::Env_) -> Result2_ {
@@ -87,15 +102,9 @@ pub fn hello3__(src:&str, fit:impl Fn(&mut IsText_), cache:bool, env:&code_::Env
 		codes = Some(Rc_::new(codes2));
 	}
 	let codes = codes.unwrap();
-	result2_::item__(code_::hello__(&codes, env), |ret| {
-		if let Err((jump_::RETURN_, s, _)) = &ret {
-			if as_ref__!(env.w).dbg_.lc_ {
-				lc3__!("({}={:?})", s, as_ref__!(env.q).name_);
-			}
-			if s.is_empty() || as_ref__!(env.q).name_.contains(s) {
-				return ok__()
-			}
-		}
-		ret
+	result2_::item__(code_::hello__(&codes, &code_::Env_::new3(
+		code_::Opt_ {guandao_du_:env.gd.guandao_du_, guandao_jie_:env.gd.guandao_jie_, ..Default::default()},
+		env)), |ret| {
+		return__(ret, env)
 	})
 }
